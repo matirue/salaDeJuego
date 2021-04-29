901be0e55129155e99bef58ad6a52fc8326a2772
matirue <matiasrue@gmail.com>
2021-04-29 03:49:00 -0300

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,17 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  //production: false,
-  production: true,
-  
+  production: false,
+
   firebase: {
     apiKey: "AIzaSyC7OcFwNqT7C6EERY30l-qXdxg9iAJg5IM",
-  authDomain: "tp-lab4-saladejuego.firebaseapp.com",
-  databaseURL: "https://tp-lab4-saladejuego-default-rtdb.firebaseio.com",
-  projectId: "tp-lab4-saladejuego",
-  storageBucket: "tp-lab4-saladejuego.appspot.com",
-  messagingSenderId: "941503226822",
-  appId: "1:941503226822:web:6a152d3785c85c28b248c7"
+    authDomain: "tp-lab4-saladejuego.firebaseapp.com",
+    databaseURL: "https://tp-lab4-saladejuego-default-rtdb.firebaseio.com",
+    projectId: "tp-lab4-saladejuego",
+    storageBucket: "tp-lab4-saladejuego.appspot.com",
+    messagingSenderId: "941503226822",
+    appId: "1:941503226822:web:6a152d3785c85c28b248c7"
   }
 };
 
