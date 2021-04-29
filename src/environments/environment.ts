5ab577d4b4ab552c43e5224e821e6a16041b367d
matirue <matiasrue@gmail.com>
2021-04-29 03:17:24 -0300

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,12 @@ export const environment = {
   production: false,
   firebaseConfig: {
     apiKey: "AIzaSyC7OcFwNqT7C6EERY30l-qXdxg9iAJg5IM",
-    authDomain: "tp-lab4-saladejuego.firebaseapp.com",
-    databaseURL: "https://tp-lab4-saladejuego-default-rtdb.firebaseio.com",
-    projectId: "tp-lab4-saladejuego",
-    storageBucket: "tp-lab4-saladejuego.appspot.com",
-    messagingSenderId: "941503226822",
-    appId: "1:941503226822:web:6a152d3785c85c28b248c7"
+  authDomain: "tp-lab4-saladejuego.firebaseapp.com",
+  databaseURL: "https://tp-lab4-saladejuego-default-rtdb.firebaseio.com",
+  projectId: "tp-lab4-saladejuego",
+  storageBucket: "tp-lab4-saladejuego.appspot.com",
+  messagingSenderId: "941503226822",
+  appId: "1:941503226822:web:6a152d3785c85c28b248c7"
   }
 };
 
